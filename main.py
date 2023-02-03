b7bb9ad7c4b17502c094ecf97b00f1b3c5bbff25
Max Wind(WIN) <mswork@e.mail.de>
2023-02-03 14:03:31 +0100

    init

---
+++ main.py
@@ -0,0 +1,195 @@
+## checks for changes, marks unwanted groups (NT& smaller then defined group size), 
+## creates new groups if not assigned
+
+import pywinauto
+import time
+import numpy as np
+import yaml
+from new_groups import create_groups, assign_participants,room_buttons_only, create_new_groups
+# get the start time
+st = time.time()
+
+def get_settings():
+    try:
+        with open('settings.txt') as f:
+            settings = yaml.safe_load(f)
+    except:
+        print("Error loading settings, loading defaults")
+        settings = {# Version 0.1
+                    "group_size" : 3,
+                    "minimal_group" : 4,
+                    "placeholder_rooms" : 5,
+                    "activate_language1" : True,
+                    "activate_language2" : True,
+                    "add_universal_to_language1" : False,
+                    "add_universal_to_language2" : False,
+                    "tags_nt":["Triad", "TRIAD", "NT", "triad","tirad","^nt "],
+                    "tags_hosts":["Host", ".:.", "Team"],
+                    "tags_lang1":["DE", "De-","De ","^de ","^de/","D E "],
+                    "tags_lang2":["EN", "En-", "En ", "ES","SP"]}
+        with open('settings.txt', 'w') as f:
+            yaml.dump(settings, f, sort_keys=False, default_flow_style=False)
+
+
+    global group_size
+    global minimal_group
+    global placeholder_rooms
+    global toggle_language
+    global add_universal_to_language
+    global tags_nt
+    global tags_hosts
+    global tags_lang1
+    global tags_lang2
+    
+
+    group_size = settings["group_size"]
+    minimal_group = settings["minimal_group"]
+    placeholder_rooms = settings["placeholder_rooms"]
+    toggle_language = [settings["activate_language1"],settings["activate_language2"]]
+    add_universal_to_language = [settings["add_universal_to_language1"],settings["add_universal_to_language2"]]
+    tags_nt = settings["tags_nt"]
+    tags_hosts = settings["tags_hosts"]
+    tags_lang1 = settings["tags_lang1"]
+    tags_lang2 = settings["tags_lang2"]
+    
+    return settings
+
+def get_breakout_participants_list(breakout_window):
+    participant_list = breakout_window.descendants(control_type="ListItem")
+
+    breakout_participants_list = []
+    for i in range(len(participant_list)):
+        parent = participant_list[i]
+        child = parent.children()[0]
+        child_child = child.children()[0]
+        child_child_child = child_child.children_texts()[0]
+        breakout_participants_list.extend([child_child_child])
+    return breakout_participants_list
+
+
+
+# main program
+
+def main():
+    
+    #initialize the breakout window
+
+    app = pywinauto.Application(backend="uia").connect(
+        title_re="Breakout Sessions - Nicht begonnen")
+
+    breakout_window = app.window(
+        title="Breakout Sessions - Nicht begonnen").wrapper_object()
+    et1 = time.time()
+
+    assigned = breakout_window.descendants(control_type="TreeItem")
+    # variables
+    name_list = []
+    last_room_nr = -1
+    rooms_to_be_cleaned = []
+    room_nr = 0
+
+
+    for i in range(len(assigned)):
+        info = assigned[i]._element_info.name
+        x = info.split(", ")
+        level = [int(s) for s in x[0].split() if s.isdigit()][0]
+        name = x[1]
+
+        if level == 0:
+            room_nr += 1
+            participants_in_room = 0
+        else:
+            participants_in_room = participants_in_room + 1
+            name_list.extend([[room_nr, participants_in_room, x[1]]])
+            for y in range(participants_in_room):
+                name_list[-y-1][1] = participants_in_room
+            # get  [Roomnr, participants_in_room]
+
+    # name_list = [[8, 2, 'NT 9r9j5'], [8, 2, 'NT t8nx9'], [9, 3, '7t774'], [9, 3, 'NT j3fch'], [9, 3, 'q0y23'], [10, 1, 'qynmj'], [11, 3, '46rmz'], [11, 3, '6w57y'], [11, 3, '7y8ex']]
+    # name_list = [[ROOM, PARTICIPANTS_SUM, NAME],...]
+
+    name_list_only = []  # = [[NAME],...]
+    rooms_to_be_cleaned = []  # = [[ROOM, PARTICIPANTS_SUM],...]
+    empty_rooms = []
+    for x in range(len(name_list)):
+        # create a name-only list
+        name_list_only.extend([name_list[x][2]])
+        # add single-participant-rooms to no_triad_rooms
+        if name_list[x][1] < group_size:  # mark room to be cleaned if room < then group_size
+            rooms_to_be_cleaned.extend([[name_list[x][0], name_list[x][1]]])
+    #  TODO: check for added names from changes in active breakout.py since new_groups.py
+    if name_list_only != []:
+        NT_ids = np.flatnonzero(np.chararray.find(name_list_only, "triad") != -1)
+        NT_ids = np.append(NT_ids,np.flatnonzero(np.chararray.find(name_list_only, "NT")!= -1))
+    else:
+        # if all rooms are empty
+        create_new_groups(group_size,placeholder_rooms)
+
+    last_room = room_nr
+    first_empty_room = name_list[-1][0]+1  # last full room + 1
+    empty_rooms.extend(range(first_empty_room, last_room))
+
+    if NT_ids.size != 0:
+        for x in NT_ids:
+            rooms_to_be_cleaned.extend([[name_list[x][0], name_list[x][1]]])
+        # no_triad_rooms = no_triad_rooms[(2 < no_triad_rooms)] # remove 1&2 room from rooms to be purged
+    else: 
+        print("no changes in rooms were found, searching for new participants...") 
+
+    # CLEAN ROOMS
+    rooms_to_be_cleaned = np.unique(rooms_to_be_cleaned, axis=0).flatten()
+    for x in range(0, len(rooms_to_be_cleaned), 2):
+        # put buttons in cache:
+        room_buttons = room_buttons_only(breakout_window)  # [1] = room 1
+        participant_buttons = breakout_window.descendants(control_type="CheckBox") 
+
+        room_buttons[rooms_to_be_cleaned[x]].click() 
+        participant_buttons = breakout_window.descendants(control_type="CheckBox") 
+        i=1 
+        while i <= rooms_to_be_cleaned[x+1]: 
+            participant_buttons[i-1].toggle() 
+            i = i + 1 
+    # safe empty rooms 
+    for room in rooms_to_be_cleaned[::2].tolist():
+        if room > 2:
+            empty_rooms.insert(0, room) # do not add participants to rooms 1 & 2 
+
+    # fill empty rooms and beyond 
+    room_buttons = room_buttons_only(breakout_window) # [1] = room 1 
+
+    room_buttons[empty_rooms[0]].click() 
+    participant_list = get_breakout_participants_list(breakout_window) 
+
+    if participant_list != []: 
+        hosts, notriad, participants_in_rooms = create_groups(participant_list) 
+
+        for i in range(len(participants_in_rooms)): 
+            try: 
+                room_buttons[empty_rooms[i]].click() # [1] = room 1 
+            except: 
+                print("no more free rooms left, please edit manually") 
+            participant_ids = participants_in_rooms[i] 
+            assign_participants(breakout_window, participant_ids) 
+
+        # assign NT 
+        room_buttons[2].click() # [2] = room 2 
+        assign_participants(breakout_window, notriad) 
+
+    else: 
+        print("no new Participants") 
+
+
+
+    # get the end time 
+    et = time.time() 
+    # get the execution time 
+    elapsed_time = et - st 
+    print('End:', elapsed_time, 'seconds') 
+    # get the execution time 
+    elapsed_time = et1 - st 
+    print('Execution time 1:', elapsed_time, 'seconds') 
+    print()
+
+if __name__ == "__main__":
+    get_settings()
+    main()
---
+++ new_groups.py
@@ -0,0 +1,329 @@
+# creates tagged breakout groups (AFK, Hosts, Language 1, Language 2)
+# preparation: open breakout window with max amount of rooms
+
+import re
+import time
+
+import numpy as np
+import pyautogui
+import pyperclip
+import pywinauto
+from pywinauto.keyboard import send_keys
+import yaml
+
+def get_settings():
+    try:
+        with open('settings.txt') as f:
+            settings = yaml.safe_load(f)
+    except:
+        print("Error loading settings, loading defaults")
+        settings = {# Version 0.1
+                    "group_size" : 3,
+                    "minimal_group" : 4,
+                    "placeholder_rooms" : 5,
+                    "activate_language1" : True,
+                    "activate_language2" : True,
+                    "add_universal_to_language1" : False,
+                    "add_universal_to_language2" : False,
+                    "tags_nt":["Triad", "TRIAD", "NT", "triad","tirad","^nt "],
+                    "tags_hosts":["Host", ".:.", "Team"],
+                    "tags_lang1":["DE", "De-","De ","^de ","^de/","D E "],
+                    "tags_lang2":["EN", "En-", "En ", "ES","SP"]}
+        with open('settings.txt', 'w') as f:
+            yaml.dump(settings, f, sort_keys=False, default_flow_style=False)
+
+
+    global group_size
+    global minimal_group
+    global placeholder_rooms
+    global toggle_language
+    global add_universal_to_language
+    global tags_nt
+    global tags_hosts
+    global tags_lang1
+    global tags_lang2
+    
+
+    group_size = settings["group_size"]
+    minimal_group = settings["minimal_group"]
+    placeholder_rooms = settings["placeholder_rooms"]
+    toggle_language = [settings["activate_language1"],settings["activate_language2"]]
+    add_universal_to_language = [settings["add_universal_to_language1"],settings["add_universal_to_language2"]]
+    tags_nt = settings["tags_nt"]
+    tags_hosts = settings["tags_hosts"]
+    tags_lang1 = settings["tags_lang1"]
+    tags_lang2 = settings["tags_lang2"]
+    
+    return settings
+
+
+
+def get_breakout_participants_list(breakout_window):
+    participant_list = breakout_window.descendants(control_type="ListItem")
+    breakout_participants_list = []
+    for i in range(len(participant_list)):
+        parent = participant_list[i]
+        child = parent.children()[0]
+        child_child = child.children()[0]
+        child_child_child = child_child.children_texts()[0]
+        breakout_participants_list.extend([child_child_child])
+    return breakout_participants_list
+
+
+def find_name(participant_list, name):
+    id_list = []
+    name_list = []
+    # find names with tags
+    for i in range(len(name)):
+        r = re.compile(name[i])
+        matches = list(filter(r.findall, participant_list)) # Read Note below
+        name_list.extend(matches)
+    return np.unique(name_list)
+
+
+def shuffle(array):
+    rnd = np.random.default_rng().permuted(array)
+    return rnd
+
+
+def split_into_groups_of(lst, group_size):
+    if lst.size != 0:
+        rest = len(lst) % group_size
+        empty_seats = group_size - rest if rest != 0 else 0
+        round_list = np.concatenate((np.full(empty_seats, np.inf), lst))
+        split_list = np.split(round_list, len(round_list)/group_size)
+    else:
+        split_list = [np.full(group_size, np.inf)]
+    return split_list
+
+
+def create_groups(participant_list):
+
+    notriad = find_name(participant_list, tags_nt)
+    hosts = find_name(participant_list, tags_hosts)
+    lang1 = find_name(participant_list, tags_lang1)
+    lang2 = find_name(participant_list, tags_lang2)
+
+    # filter Universal
+    universal = np.intersect1d(lang1, lang2)
+    # clean out hosts universal and NTs
+    lang1_clean = np.setdiff1d(
+        lang1, np.concatenate((universal, notriad, hosts)))
+    lang2_clean = np.setdiff1d(
+        lang2, np.concatenate((universal, notriad, hosts)))
+
+    no_tag = np.setdiff1d(participant_list, np.concatenate(
+        (universal, notriad, hosts, lang1_clean, lang2_clean)))
+    notag_uni = np.concatenate((universal, no_tag))
+    # distribute universal
+    if np.sum(add_universal_to_language) != 0:
+        uni_split = np.array_split(
+            shuffle(notag_uni), np.sum(add_universal_to_language))
+            
+    if add_universal_to_language[0]:
+        lang1_final = np.concatenate((lang1_clean, uni_split[0]))
+    else:
+        lang1_final = lang1_clean
+
+    if add_universal_to_language[1]:
+        if np.sum(add_universal_to_language) == 1:
+            lang2_final = np.concatenate((lang2_clean, uni_split[0]))
+        elif np.sum(add_universal_to_language) == 2:
+            lang2_final = np.concatenate((lang2_clean, uni_split[1]))
+    else:
+        lang2_final = lang2_clean
+
+    # split
+    lang1_split = split_into_groups_of(shuffle(lang1_final), group_size)
+    lang2_split = split_into_groups_of(shuffle(lang2_final), group_size)
+    # use languages according to toggle_language
+    participants_in_groups = []
+    if toggle_language[0]:
+        participants_in_groups.extend(lang1_split)
+    if toggle_language[1]:
+        participants_in_groups.extend(lang2_split)
+
+    # print(participants_in_groups)
+    return hosts, notriad, participants_in_groups
+
+
+def send_keys_fast(value):
+    prev_value = pyperclip.paste()
+    pyperclip.copy(value)
+    send_keys("^v")
+    pyperclip.copy(prev_value)
+
+
+def starting_position(breakout_window):
+    rect = breakout_window.Raum1Static.parent().rectangle()
+    breakout_window.set_focus()
+    pywinauto.mouse.click(
+        coords=(rect.left+10, round(rect.top+(rect.bottom-rect.top)/2)))
+
+
+def rename_room(name):
+    send_keys("{TAB}{SPACE}")
+    send_keys_fast(name)
+    send_keys("{ENTER}{TAB}")
+
+
+# only room buttons accessable for more security
+def room_buttons_only(breakout_window):
+    room_buttons = breakout_window.descendants(
+        control_type="Button")  # [1] = room 1
+    mark_to_del = []
+    for i in range(0, len(room_buttons)-1):
+        name = room_buttons[i]._element_info.name
+        if name == "Umbenennen":
+            mark_to_del.extend([i, i+1])  # & next Button "Löschen"
+    for i in sorted(mark_to_del, reverse=True):
+        del room_buttons[i]
+    # add something to index 0 to have same id in id&roomnr
+    room_buttons = ["close button disabled"]+room_buttons[1:-2]
+    return room_buttons
+
+
+def assign_participants(breakout_window, participants_arr):
+    # start participants list
+    # send_keys("{TAB}{TAB}{TAB}{SPACE}")
+    participant_list = get_breakout_participants_list(breakout_window)
+    participant_buttons = breakout_window.descendants(control_type="CheckBox")
+    empty_seat = 0
+    for name in participants_arr:
+        if name != "inf" and isinstance(name, str):
+            id = np.flatnonzero(np.chararray.find(participant_list, name) != -1)
+            for x in id:  # if a name is doubled use only first
+                participant_buttons[int(x)].toggle()
+                break
+        else:
+            empty_seat = empty_seat + 1
+            if group_size - empty_seat < minimal_group:
+                break
+
+# return to starting position
+    # send_keys("{ESC}+{TAB}+{TAB}+{TAB}")
+
+
+def next_room():
+    # focus next room and collaps
+    send_keys("{DOWN}{LEFT}")
+
+
+def update_list_positions(id_array, removed_ids):
+    for id in removed_ids:
+        if not np.isinf(id):
+            filter_arr = (id_array > id).astype(int)
+            id_array = id_array-filter_arr
+    return id_array
+
+
+def breakout_assignment(hosts, notriad, participants_in_rooms, placeholder_rooms, breakout_window):
+    # starting_position(breakout_window)
+
+    # TODO navigate through click on zuordnen and move down if it is not found
+    # breakout_buttons = breakout_window.descendants(control_type="Button") # [1] = room 1
+    # breakout_buttons[1].click()
+    breakout_buttons = room_buttons_only(breakout_window)  # [1] = room 1
+
+    # main loop
+    row = 0
+    for room in range(2+len(participants_in_rooms)):  # specialrooms+participant_rooms
+        if room == 0:
+            breakout_buttons[room+1].click()  # [1] = room 1
+            # rename_room("Teamroom")
+            assign_participants(breakout_window, hosts)
+            #notriad = update_list_positions(notriad,hosts)
+            #participants_in_rooms = update_list_positions(participants_in_rooms,hosts)
+            # next_room()
+            # print(str(hosts).encode(sys.stdout.encoding, errors='replace'))
+
+        elif room == 1:
+            breakout_buttons[room+1].click()  # [1] = room 1
+            #rename_room("No Triad")
+            assign_participants(breakout_window, notriad)
+            #participants_in_rooms = update_list_positions(participants_in_rooms,notriad)
+            # next_room()
+            # print(str(notriad).encode(sys.stdout.encoding, errors='replace'))
+
+        # elif room == 2:
+        #     for i in range(placeholder_rooms):
+        #         next_room()
+        #         print("jump over")
+        #     # no clue why it is needed but this is the only way the first room collapses
+        #     send_keys("{LEFT}")
+        else:
+            breakout_buttons[room+placeholder_rooms+1].click()  # [1] = room 1
+            participant_ids = participants_in_rooms[row]
+            assign_participants(breakout_window, participant_ids)
+            #participants_in_rooms = update_list_positions(participants_in_rooms,participant_ids)
+            # print(participant_ids)
+            row = row + 1
+            # send_keys("{LEFT}")
+            # next_room()
+
+    # print(str(participants_in_rooms).encode(sys.stdout.encoding, errors='replace'))
+
+
+def create_new_rooms(breakout_window):
+    if pyautogui.confirm(text='Do you want to replace your current breakouts?', title='Creating Breakouts', buttons=['OK', 'Cancel']) == 'OK':
+        breakout_buttons_2 = breakout_window.descendants(
+            control_type="Button")  # [1] = room 1
+        breakout_buttons_2[-3].click()
+        breakout_buttons_2 = breakout_window.descendants(
+            control_type="Button")  # [1] = room 1
+        breakout_buttons_2[0].click()
+    else:
+        exit()
+
+
+def create_new_groups(group_size, placeholder_rooms):
+    # Initiate
+    try:
+        app = pywinauto.Application(backend="uia").connect(
+            title_re="Breakout Sessions - Nicht begonnen")
+    except:
+        print("Error: Please open breakout window!")
+        exit()
+    breakout_window = app.window(
+        title="Breakout Sessions - Nicht begonnen").wrapper_object()
+    create_new_rooms(breakout_window)
+
+    # get the start time
+    st = time.time()
+    # get all buttons
+    breakout_buttons = room_buttons_only(breakout_window)  # [1] = room 1
+    breakout_buttons[1].click()
+    participant_list = get_breakout_participants_list(breakout_window)
+    # participant_list = np.load("300_participants_coregroup.npy")
+    hosts, notriad, participants_in_rooms = create_groups(participant_list)
+    print()
+    et1 = time.time()
+    # get the start time
+    st2 = time.time()
+    breakout_assignment(hosts, notriad, participants_in_rooms,
+                        placeholder_rooms, breakout_window)
+    # get the end time
+    et2 = time.time()
+    # get the end time
+    et = time.time()
+
+    # get the first execution time
+    elapsed_time = et1 - st
+    print('Execution time creating groups:', elapsed_time, 'seconds')
+    # get the first execution time
+    elapsed_time = et2 - st2
+    print('Execution time assigning:', elapsed_time, 'seconds')
+
+    # get complete execution time
+    elapsed_time = et - st
+    print('End:', elapsed_time, 'seconds')
+    print()
+
+
+if __name__ == "__main__":
+    get_settings()
+    create_new_groups(group_size, placeholder_rooms)
+
+    # participant_list = np.load("300_participants_coregroup.npy")
+    # hosts, notriad, participants_in_rooms = create_groups(participant_list)
+    # print(participants_in_rooms)
