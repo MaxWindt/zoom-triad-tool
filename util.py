19a389f2f0443c09d2e494eee865566498e9dc78
MaxWindt <86522026+MaxWindt@users.noreply.github.com>
2024-02-16 14:17:43 +0100

    formatting

--- util.py
+++ util.py
@@ -24,10 +24,12 @@ def send_text_to_zoom(text):
     try:
         # initialize the breakout window
         app = pywinauto.Application(backend="uia").connect(
-            title_re="Breakout Sessions - Im Gange.*")
+            title_re="Breakout Sessions - Im Gange.*"
+        )
 
         app_wrapper = app.window(
-            title_re="Breakout Sessions - Im Gange.*").wrapper_object()
+            title_re="Breakout Sessions - Im Gange.*"
+        ).wrapper_object()
         name = app_wrapper._element_info.name
         app_buttons = app_wrapper.descendants(control_type="Button")
         sending_text_btn = app_buttons[-2]
@@ -60,7 +62,7 @@ def make_a_sound():
     pygame.init()
     pygame.mixer.init()
     sound = pygame.mixer.Sound("zimbeln.mp3")
-    sound.set_volume(0.5)   # Now plays at 50% of full volume.
+    # sound.set_volume(0.5)   # Now plays at 50% of full volume.
     sound.play()
 
 
@@ -68,10 +70,16 @@ def share_external_audio():
     pywinauto.keyboard.send_keys("%s")
     try:
         app = pywinauto.Application(backend="uia").connect(
-            title="Wählen Sie ein Fenster oder eine Anwendung, die Sie freigeben möchten")
+            title="Wählen Sie ein Fenster oder eine Anwendung, die Sie freigeben möchten"
+        )
         pos = pyautogui.position()
-        sharing_window = app.window(
-            title="Wählen Sie ein Fenster oder eine Anwendung, die Sie freigeben möchten").ListItem5.set_focus().click_input()
+        sharing_window = (
+            app.window(
+                title="Wählen Sie ein Fenster oder eine Anwendung, die Sie freigeben möchten"
+            )
+            .ListItem5.set_focus()
+            .click_input()
+        )
         pyautogui.moveTo(pos)
         # send_keys('{ENTER}')
     except:
@@ -87,24 +95,26 @@ def get_time_left_in_breakouts():
     try:
         # initialize the breakout window
         app = pywinauto.Application(backend="uia").connect(
-            title_re="Breakout Sessions - Im Gange.*")
+            title_re="Breakout Sessions - Im Gange.*"
+        )
 
         app_wrapper = app.window(
-            title_re="Breakout Sessions - Im Gange.*").wrapper_object()
+            title_re="Breakout Sessions - Im Gange.*"
+        ).wrapper_object()
         name = app_wrapper._element_info.name
 
         # Find the position of the opening parenthesis
-        parenthesis_index = name.find('(')
+        parenthesis_index = name.find("(")
 
         # If opening parenthesis is found
         if parenthesis_index != -1:
             # Split the string based on parentheses
-            split_text = name.split('(')
+            split_text = name.split("(")
 
             # Get the second element (which contains the time) and remove the closing parenthesis
-            time_string = split_text[1].replace(')', '')
+            time_string = split_text[1].replace(")", "")
             # Split the time string based on colons
-            time_components = time_string.split(':')
+            time_components = time_string.split(":")
         else:
             time_components = "0"
     except:
@@ -114,7 +124,7 @@ def get_time_left_in_breakouts():
     return time_components
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     what = get_time_left_in_breakouts()
     print(what)
     send_text_to_zoom("what")
